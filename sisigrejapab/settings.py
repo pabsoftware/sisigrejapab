fe14ff6a7f2c2cf15c6258ff87e95e7d12689c4b
pabsoftware <yopabsoftware@outlook.com>
2022-10-02 09:45:17 -0300

    remoção app cong

--- sisigrejapab/settings.py
+++ sisigrejapab/settings.py
@@ -43,7 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    #'terceiro'
     'bootstrap5',
+
+    #local
     'usuarios',
     'core',
     'cadastros',
